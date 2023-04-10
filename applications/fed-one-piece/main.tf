c7ba2cbffa7f499f49d784dda0ff7779fd0cadb3
Charles Cheng <csuchq@hotmail.com>
2023-04-11 09:10:58 +1000

    Back up FED one piece code

---
+++ applications/fed-one-piece/main.tf
@@ -0,0 +1,18 @@
+# module "fed-s3" {
+#   source = "../../modules/s3"
+
+#   domain_name_dev = var.my_domain_name
+# }
+
+module "fed-route53" {
+  source = "../../modules/route53"
+
+  domain_name = var.my_domain_name
+}
+
+module "fotopie-fed-vpc" {
+  source = "../../modules/vpc"
+
+  application_name = var.application_name
+}
+
